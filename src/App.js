2207ca4afa12c2efef7974cd4b8e9a967f5bbba0
Michał Hołowaty <m.holowaty@outlook.com>
2024-07-29 16:49:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 function App() {
   const value = 'World, this is a test!';
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/getreacttest`)).json();
+      setData(text);
+    })();
+  });
   return <div>Hello {value}</div>;
 }
 
